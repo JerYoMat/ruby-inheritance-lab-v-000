97fd9421ba0431f699c5465536a60f0cc5a62844
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-08 13:56:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,16 +2,14 @@
 
 class Student < User 
   attr_accessor :knowledge
-  KNOWLEDGE = []
+ 
 
   def initialize
-  @knowledge = KNOWLEDGE
+  @knowledge = []
 
   def learn(string)
-    KNOWLEDGE << string 
+    @knowledge << string 
   end 
-  def knowledge 
-    KNOWLEDGE
-  end 
+ 
 
 end
